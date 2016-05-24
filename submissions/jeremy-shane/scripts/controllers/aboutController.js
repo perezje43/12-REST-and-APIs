40f767549245c570b1a0055312aa8bf49ad35620
Jeremy Perez <perezje43@gmail.com>
2016-05-24 12:51:46 -0700

    completed MVP

---
+++ submissions/jeremy-shane/scripts/controllers/aboutController.js
@@ -0,0 +1,15 @@
+(function(module) {
+  var aboutController = {};
+
+  aboutController.index = function() {
+    $('#about').show().siblings().hide();
+
+    // DONE: Call the function that loads (or 'requests') our repo data.
+    //  Pass in some view function as a higher order callback, so our repos
+    //  will render after the data is loaded.
+    repos.requestRepos(repoView.index);
+
+  };
+
+  module.aboutController = aboutController;
+})(window);
